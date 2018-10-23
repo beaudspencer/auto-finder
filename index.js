34f8ff087eb3954ca745b878899e7bcbf1e55837
Beau Spencer <bdspencer416@gmail.com>
2018-10-23 12:49:26 -0700

    refactored backend to rpoperly upload files to s3. #1

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 require('dotenv/config')
 
 const express = require('express')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const AWS = require('aws-sdk')
+
+const albumBucketName = 'auto-finder'
+const bucketRegion = 'us-east-1'
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: {
+    accessKeyId: 'AKIAICV3K4RZ5ZWLM2FQ',
+    secretAccessKey: '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'
+  }
+})
+
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: albumBucketName}
+})
+
+// const IAM-SECRET-KEY = RuUDQqMstOKEtgtghA4dUpJaKOGhtz2M7UpGCEl2
+// const IAM-KEY = AKIAICV3K4RZ5ZWLM2FQ
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'auto-finder',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname})
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
 const app = express()
 
-app.use('/', express.static('public'))
+app.get('/', express.static('public'))
 
-app.post('/', (req, res) => {
-  console.log('uploading')
+app.post('/', upload.single('image'), (req, res, next) => {
+  res.send(req.file.location)
 })
 
 app.listen(process.env.PORT, () => {
