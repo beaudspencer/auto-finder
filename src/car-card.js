ff4ae2f5faaab77d61378e4678362f7b69319320
Beau Spencer <bdspencer416@gmail.com>
2018-10-25 15:44:13 -0700

    added find listings button to car card that calls pull listings method. #3

--- src/car-card.js
+++ src/car-card.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
 const styles = {
@@ -17,38 +19,52 @@ const styles = {
   }
 }
 
-export default function CarCard(props) {
-  return (
-    <div style={styles.div}>
-      <Card style={styles.card}>
-        <CardMedia
-          style={styles.media}
-          component="img"
-          alt="Car"
-          image={props.car.imageURL}
-        />
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h4"
-          >
-            {props.car.model}
-          </Typography>
-          <Typography
-            color="textSecondary"
-            variant="h6"
-            gutterBottom
-          >
-            {'Make: ' + props.car.make}
-          </Typography>
-          <Typography
-            color="textSecondary"
-            variant="h6"
-          >
-            {'Year: ' + props.car.model_year}
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
-  )
+export default class CarCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+  handleSearch() {
+    this.props.search(this.props.car)
+  }
+  render() {
+    return (
+      <div style={styles.div}>
+        <Card style={styles.card}>
+          <CardMedia
+            style={styles.media}
+            component="img"
+            alt="Car"
+            image={this.props.car.imageURL}
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h4"
+            >
+              {this.props.car.model}
+            </Typography>
+            <Typography
+              color="textSecondary"
+              variant="h6"
+              gutterBottom
+            >
+              {'Make: ' + this.props.car.make}
+            </Typography>
+            <Typography
+              color="textSecondary"
+              variant="h6"
+            >
+              {'Year: ' + this.props.car.model_year}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button onClick={this.handleSearch}>
+              Find Listings
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    )
+  }
 }
