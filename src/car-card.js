52e9c70ee9a6694831e4d8d2be723c02eaeab603
Beau Spencer <bdspencer416@gmail.com>
2018-10-31 11:09:38 -0700

    refactored button styling to be an icon and adjusted loading symbol to be positioned over the button. #5

--- src/car-card.js
+++ src/car-card.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
-import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
 import {withStyles} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 import Typography from '@material-ui/core/Typography'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
@@ -20,15 +20,28 @@ const styles = {
   },
   media: {
     objectFit: 'cover'
+  },
+  search: {
+    position: 'relative',
+    marginTop: '0.33rem'
   }
 }
 
 const SearchButton = withStyles({
   root: {
     position: 'relative',
-    right: '0.5rem'
+    right: '-1.5rem',
+    bottom: '1rem'
   }
 })(Button)
+
+const SearchLoader = withStyles({
+  root: {
+    position: 'absolute',
+    left: '1.35rem',
+    bottom: '0.8rem'
+  }
+})(CircularProgress)
 
 export default class CarCard extends React.Component {
   constructor(props) {
@@ -79,12 +92,16 @@ export default class CarCard extends React.Component {
                 {'Year: ' + this.props.car.model_year}
               </Typography>
             </CardContent>
-            <CardActions>
-              <SearchButton onClick={this.handleSearch}>
-                {this.state.loading ? 'searching...' : 'Find Listings'}
+            <div style={styles.search}>
+              <SearchButton
+                onClick={this.handleSearch}
+                variant="fab"
+                color="primary"
+              >
+                <SearchIcon/>
               </SearchButton>
-              {this.state.loading && <CircularProgress color="secondary" size={30}/>}
-            </CardActions>
+              {this.state.loading && <SearchLoader color="secondary" size={60}/>}
+            </div>
           </Card>
         </div>}
         {!this.props.car && <div>
@@ -92,7 +109,7 @@ export default class CarCard extends React.Component {
             variant="title"
             component="h2"
           >
-            You have yet find a car this session
+            You have yet to find a car this session
           </Typography>
         </div>}
       </React.Fragment>
