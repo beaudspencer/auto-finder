01049e0eb084bf5d40b53f912269c81d3fc6a2bd
beaudspencer <42357176+beaudspencer@users.noreply.github.com>
2018-11-13 19:16:37 -0800

    defined a FilterMenu component that render a FilterBy menu with a has picture filter. #13

---
+++ src/filter-menu.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+
+export default class FilterMenu extends React.Component {
+  state = {
+    anchorEl: null
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget })
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null })
+  }
+
+  render() {
+    const { anchorEl } = this.state
+
+    return (
+      <div>
+        <Button
+          aria-owns={anchorEl ? 'FilterBy' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          Open Menu
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={this.handleClose}>Has Picture</MenuItem>
+        </Menu>
+      </div>
+    )
+  }
+}
