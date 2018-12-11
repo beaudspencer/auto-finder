ee1ab8878225a605409494cc8ee264494e639972
beaudspencer <42357176+beaudspencer@users.noreply.github.com>
2018-12-10 18:23:32 -0800

    added click listener that calls the search method. added a loading property to the component state that gets set on search method call. add conditionally rendered loading circle. #10

--- src/recent-card.js
+++ src/recent-card.js
@@ -5,11 +5,13 @@ import {
   CardMedia,
   Typography,
   withStyles,
-  ButtonBase
+  ButtonBase,
+  CircularProgress
 } from '@material-ui/core'
 
 const RecentsCard = withStyles({
   root: {
+    position: 'relative',
     display: 'inline-block',
     margin: '0 auto',
     width: '50%',
@@ -24,39 +26,70 @@ const Instr = withStyles({
   }
 })(Typography)
 
-export default function RecentCard(props) {
-  const { car } = props
-  return (
-    <RecentsCard>
-      <ButtonBase
-        focusRipple
-      >
-        <CardMedia
-          component="img"
-          image={car.imageURL}
-          title={car.model}
-        />
-      </ButtonBase>
-      <CardContent>
-        <Instr
-          variant="caption"
-          component="p"
+const SearchLoading = withStyles({
+  root: {
+    position: 'absolute',
+    left: '5rem',
+    top: '3rem',
+    zIndex: '2'
+  }
+})(CircularProgress)
+
+export default class RecentCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false
+    }
+    this.search = this.search.bind(this)
+  }
+  search() {
+    const { search, car } = this.props
+    search(car)
+    this.setState({
+      loading: true
+    })
+  }
+  render() {
+    const { car } = this.props
+    return (
+      <RecentsCard>
+        {
+          this.state.loading && <SearchLoading
+            color="secondary"
+          />
+        }
+        <ButtonBase
+          onClick={this.search}
+          focusRipple
         >
-          (Click Image To Search)
-        </Instr>
-        <Typography
-          component="h6"
-          variant="h6"
-        >
-          {
-            car.make
-          }
-          <br/>
-          {
-            car.model
-          }
-        </Typography>
-      </CardContent>
-    </RecentsCard>
-  )
+          <CardMedia
+            component="img"
+            image={car.imageURL}
+            title={car.model}
+          />
+        </ButtonBase>
+        <CardContent>
+          <Instr
+            variant="caption"
+            component="p"
+          >
+            (Click Image To Search)
+          </Instr>
+          <Typography
+            component="h6"
+            variant="h6"
+          >
+            {
+              car.make
+            }
+            <br/>
+            {
+              car.model
+            }
+          </Typography>
+        </CardContent>
+      </RecentsCard>
+    )
+  }
 }
